4c4aba9e2518d6e9388a60891217a8ea36780a9a
Donald Dimitrovski <donald.dimitriovski@symphony-solutions.eu>
2018-05-26 00:29:24 +0200

    templates set-up

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,42 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
+
+class MessageForm(forms.Form):
+    text_input = forms.CharField()
+
+    textarea = forms.CharField(
+        widget=forms.Textarea(),
+    )
+
+    radio_buttons = forms.ChoiceField(
+        choices=(
+            ('option_one', "Option one is this and that be sure to include why it's great"),
+            ('option_two', "Option two can is something else and selecting it will deselect option one")
+        ),
+        widget=forms.RadioSelect,
+        initial='option_two',
+    )
+
+    checkboxes = forms.MultipleChoiceField(
+        choices=(
+            ('option_one', "Option one is this and that be sure to include why it's great"),
+            ('option_two', 'Option two can also be checked and included in form results'),
+            ('option_three', 'Option three can yes, you guessed it also be checked and included in form results')
+        ),
+        initial='option_one',
+        widget=forms.CheckboxSelectMultiple,
+        help_text="<strong>Note:</strong> Labels surround all the options for much larger click areas and a more usable form.",
+    )
+
+    appended_text = forms.CharField(
+        help_text="Here's more help text"
+    )
+
+    prepended_text = forms.CharField()
+
+    prepended_text_two = forms.CharField()
+
+    multicolon_select = forms.MultipleChoiceField(
+        choices=(('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')),
+    )
--- app/views.py
+++ app/views.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
+from .forms import MessageForm
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world")
+    form = MessageForm()
+    print(form)
+
+    return render(request, 'app/template.html', {
+        'form': form,
+    })
+    # return HttpResponse("Hello world")
