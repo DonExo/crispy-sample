490d8bd030305665222ad10758a5c4748ea50db2
Donald Dimitrovski <donald.dimitriovski@symphony-solutions.eu>
2018-05-26 00:40:09 +0200

    Applied basic crispy formatting

--- app/forms.py
+++ app/forms.py
@@ -43,3 +43,20 @@ class MessageForm(forms.Form):
     multicolon_select = forms.MultipleChoiceField(
         choices=(('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')),
     )
+
+    # Uni-form
+    helper = FormHelper()
+    helper.form_class = 'form-horizontal'
+    helper.layout = Layout(
+        Field('text_input', css_class='input-xlarge'),
+        Field('textarea', rows="3", css_class='input-xlarge'), 'radio_buttons',
+        Field('checkboxes', style="background: #FAFAFA; padding: 10px;"),
+        AppendedText('appended_text', '.00'),
+        PrependedText('prepended_text', '<input type="checkbox" checked="checked" value="" id="" name="">', active=True),
+        PrependedText('prepended_text_two', '@'),
+        'multicolon_select',
+        FormActions(
+            Submit('save_changes', 'Save changes', css_class="btn-primary"),
+            Submit('cancel', 'Cancel'),
+        )
+    )
